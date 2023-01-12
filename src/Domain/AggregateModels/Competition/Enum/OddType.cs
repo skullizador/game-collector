53a192ac59833145bf84fe895a478f7e93b2f7ec
Motinha <motinha11@outlook.pt>
2023-01-12 22:35:34 +0000

    HA-84: Add enums;

---
+++ src/Domain/AggregateModels/Competition/Enum/OddType.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// Program
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GameCollector.Domain.AggregateModels.Competition.Enum
+{
+    /// <summary>
+    /// <see cref="OddType"/>
+    /// </summary>
+    public enum OddType
+    {
+        /// <summary>
+        /// The v1
+        /// </summary>
+        V1 = 1,
+
+        /// <summary>
+        /// The x
+        /// </summary>
+        X = 2,
+
+        /// <summary>
+        /// The v2
+        /// </summary>
+        V2 = 3
+    }
+}
