8e5edae88cb4e6dc06606ccabe5e5cde47624906
Motinha <motinha11@outlook.pt>
2023-01-14 22:28:59 +0000

    HA-86: Add "Get Competition Details" feature;

---
+++ src/Presentation.WebAPI/Controllers/CompetitionController.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompetitionController.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// CompetitionController
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GameCollector.Presentation.WebAPI.Controllers
+{
+    using System.Net;
+    using AutoMapper;
+    using GameCollector.Domain.AggregateModels.Competition;
+    using GameCollector.Presentation.WebAPI.Dtos.Input.Competition;
+    using GameCollector.Presentation.WebAPI.Dtos.Output.Competition;
+    using GameCollector.Presentation.WebAPI.Queries.Competition.GetByCompetitionIdQuery;
+    using GameCollector.Presentation.WebAPI.Utils;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/v1/Competition")]
+    public class CompetitionController : Controller
+    {
+        private readonly IMapper mapper;
+
+        private readonly IMediator mediator;
+
+        public CompetitionController(
+            IMapper mapper,
+            IMediator mediator)
+        {
+            this.mapper = mapper;
+            this.mediator = mediator;
+        }
+
+        /// <summary>
+        /// Gets the by competition identifier asynchronous.
+        /// </summary>
+        /// <param name="filter">The filter.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        [HttpGet("{CompetitionId}")]
+        [ProducesResponseType(typeof(CompetitionDetailsDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetByCompetitionIdAsync([FromRoute] GetByCompetitionIdDto filter, CancellationToken cancellationToken)
+        {
+            Competition competition = await this.mediator.Send(new GetByCompetitionIdQuery
+            {
+                CompetitionId = filter.CompetitionId
+            }, cancellationToken);
+
+            return this.Ok(this.mapper.Map<CompetitionDetailsDto>(competition));
+        }
+    }
+}
