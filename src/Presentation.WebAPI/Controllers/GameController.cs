1d392b54f79301f4edb85649072d511e7e6352da
Motinha <motinha11@outlook.pt>
2023-01-15 17:49:27 +0000

    HA-91: Add "Get game details" feature;

---
+++ src/Presentation.WebAPI/Controllers/GameController.cs
@@ -0,0 +1,73 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GameController.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// GameController
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GameCollector.Presentation.WebAPI.Controllers
+{
+    using System.Net;
+    using AutoMapper;
+    using GameCollector.Domain.AggregateModels.Competition;
+    using GameCollector.Presentation.WebAPI.Dtos.Input.Competition;
+    using GameCollector.Presentation.WebAPI.Dtos.Output.Competition;
+    using GameCollector.Presentation.WebAPI.Queries.Competition.GetByGameIdQuery;
+    using GameCollector.Presentation.WebAPI.Utils;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// <see cref="GameController"/>
+    /// </summary>
+    /// <seealso cref="Controller"/>
+    [ApiController]
+    [Route("api/v1/Game")]
+    public class GameController : Controller
+    {
+        /// <summary>
+        /// The mapper
+        /// </summary>
+        private readonly IMapper mapper;
+
+        /// <summary>
+        /// The mediator
+        /// </summary>
+        private readonly IMediator mediator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameController"/> class.
+        /// </summary>
+        /// <param name="mapper">The mapper.</param>
+        /// <param name="mediator">The mediator.</param>
+        public GameController(
+            IMapper mapper,
+            IMediator mediator)
+        {
+            this.mapper = mapper;
+            this.mediator = mediator;
+        }
+
+        /// <summary>
+        /// Gets the by game identifier asynchronous.
+        /// </summary>
+        /// <param name="filters">The filters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        [HttpGet("{GameId}")]
+        [ProducesResponseType(typeof(GameDetailsDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetByGameIdAsync([FromRoute] GetByGameIdDto filters, CancellationToken cancellationToken)
+        {
+            Game game = await this.mediator.Send(new GetByGameIdQuery
+            {
+                GameId = filters.GameId
+            }, cancellationToken);
+
+            return this.Ok(this.mapper.Map<GameDetailsDto>(game));
+        }
+    }
+}
