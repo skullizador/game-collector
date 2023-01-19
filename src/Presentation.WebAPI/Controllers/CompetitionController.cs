915f330cbea106fbb31d9945efab8fe6c1873ca8
Carlos Nunes <carlosnunes__9@outlook.pt>
2023-01-19 21:47:23 +0000

    HA-89: Add Delete Competition feature;

--- src/Presentation.WebAPI/Controllers/CompetitionController.cs
+++ src/Presentation.WebAPI/Controllers/CompetitionController.cs
@@ -12,7 +12,8 @@ namespace GameCollector.Presentation.WebAPI.Controllers
     using System.Net;
     using AutoMapper;
     using GameCollector.Domain.AggregateModels.Competition;
-    using GameCollector.Presentation.WebAPI.Command.CreateCompetitionCommand;
+    using GameCollector.Presentation.WebAPI.Commands.CreateCompetitionCommand;
+    using GameCollector.Presentation.WebAPI.Commands.DeleteCompetitionCommand;
     using GameCollector.Presentation.WebAPI.Commands.UpdateCompetitionCommand;
     using GameCollector.Presentation.WebAPI.Dtos.Input.Competition;
     using GameCollector.Presentation.WebAPI.Dtos.Output.Competition;
@@ -90,21 +91,7 @@ namespace GameCollector.Presentation.WebAPI.Controllers
 
             return this.Created(string.Empty, this.mapper.Map<CompetitionDetailsDto>(competition));
         }
-
-        /// <summary>
-        /// Gets all asynchronous.
-        /// </summary>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<CompetitionDto>), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
-        {
-            IEnumerable<Competition> competitions = await this.mediator.Send(new GetAllCompetitionsQuery(), cancellationToken);
-
-            return this.Ok(this.mapper.Map<IEnumerable<CompetitionDto>>(competitions));
-        }
-
+       
         /// <summary>
         /// Gets the by competition identifier asynchronous.
         /// </summary>
@@ -151,5 +138,26 @@ namespace GameCollector.Presentation.WebAPI.Controllers
 
             return this.Ok(this.mapper.Map<CompetitionDetailsDto>(competition));
         }
+
+        /// <summary>
+        /// Deletes the competition asynchronous.
+        /// </summary>
+        /// <param name="filter">The filter.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        [HttpDelete("{CompetitionId}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> DeleteCompetitionAsync([FromRoute] GetByCompetitionIdDto filter, CancellationToken cancellationToken)
+        {
+            await this.mediator.Publish(new DeleteCompetitionCommand
+            {
+                CompetitionId = filter.CompetitionId
+            }, cancellationToken);
+
+            return this.Ok();
+        }
+        
     }
 }
