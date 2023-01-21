3459ca53145fc570909e779ec45fc46700fcff66
Carlos Nunes <carlosnunes__9@outlook.pt>
2023-01-21 21:42:11 +0000

    HA-95: Add "Create Odd in Game" feature

---
+++ src/Presentation.WebAPI/Commands/CreateOddCommand/CreateOddCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CreateOddCommandHandler.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// CreateOddCommandHandler
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GameCollector.Presentation.WebAPI.Commands.CreateOddCommand
+{
+    using GameCollector.Domain.AggregateModels.Competition;
+    using GameCollector.Domain.AggregateModels.Competition.Builder.OddBuilder;
+    using GameCollector.Domain.AggregateModels.Competition.Repository;
+    using GameCollector.Domain.Exceptions;
+    using MediatR;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class CreateOddCommandHandler : IRequestHandler<CreateOddCommand, Odd>
+    {
+        private readonly IGameRepository gameRepository;
+
+        private readonly IOddBuilder oddBuilder;
+
+        public CreateOddCommandHandler(IGameRepository gameRepository, IOddBuilder oddBuilder)
+        {
+            this.gameRepository = gameRepository;
+            this.oddBuilder = oddBuilder;
+        }
+
+        public async Task<Odd> Handle(CreateOddCommand request, CancellationToken cancellationToken)
+        {
+            Game game = await this.gameRepository.GetAsync(request.GameId, cancellationToken);
+
+            if (game is null)
+            {
+                throw new NotFoundException($"The game with id {request.GameId} wasn't found.");
+            }
+
+            this.oddBuilder.NewOdd(request.BookmakerId, request.Type, request.Value);
+             
+            if(request.TeamId is not null)
+            {
+                this.oddBuilder.AddTeamId(request.TeamId);
+            }
+
+            Odd odd = this.oddBuilder.Build();
+
+            game.AddOdd(odd);
+
+            await this.gameRepository.Update(game, cancellationToken);
+
+            await this.gameRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+            return odd;
+            
+        }
+    }
+}
---
+++ src/Presentation.WebAPI/Validation/Competition/CreateOddDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CreateOddDtoValidator.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// CreateOddDtoValidator
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GameCollector.Presentation.WebAPI.Validation.Competition
+{
+    using FluentValidation;
+    using GameCollector.Presentation.WebAPI.Dtos.Input.Competition;
+
+    public class CreateOddDtoValidator : AbstractValidator<CreateOddDto>
+    {
+        public CreateOddDtoValidator()
+        {
+            this.RuleFor(x => x.BookmakerId)
+                .NotEqual(Guid.Empty)
+                    .WithMessage("The Bookmaker Id shouldn't be empty.");
+
+            this.RuleFor(x => x.Type)
+                .NotEmpty()
+                    .WithMessage("The odd Type shouldn't be empty.");
+
+            this.RuleFor(x => x.Value)
+                .NotEmpty()
+                    .WithMessage("The odd Value shouldn't be empty.");
+
+        }
+    }
+}
